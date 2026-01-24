0bce7eb16618c4fe72d7f2987a70e2ef27927afa
Rishab0323 <srishbh574@gmail.com>
2026-01-24 09:13:58 +0530

    user req

--- Paytm-Elite/backend/routes/user.js
+++ Paytm-Elite/backend/routes/user.js
@@ -1,8 +1,45 @@
 const express = require("express");
 const router = express.Router();
+import { User } from "../database/db";
 
-router.get("/hel",(req,res) => {
-    res.send("user route is activated");
+const signupData = {
+    username : zod.string().email(),
+    password : zod.string(),
+    firstname : zod.string(),
+    lastname : zod.string()
+}
+
+router.get("/signup",(req,res) => {
+    const {successData} = signupData.safeParse(req.body)
+    if(!successData){
+        return res.status(411).json({message : "Email already exist / invalid input"});
+    }
+
+    const userExist = User.findOne({
+        username : req.body.username
+    });
+
+    if(userExist){
+        return res.status(411).json({msg : " user already exist"});
+    }
+
+    const user = User.create({
+        username : req.body.username,
+        password : req.body.password,
+        firstname : req.body.firstname,
+        lastname : req.body.lastname,
+    })
+
+    const userId = user._id;
+
+    const token = jwt.sign({
+        userId
+    }.JWT_SECRET);
+
+    res.send({
+        message : "user created succesfully",
+        token : token
+    })
 
 })
 
