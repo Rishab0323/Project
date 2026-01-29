b1112e94312089d12c46693ab09c6c5a68618c6d
Rishab0323 <srishbh574@gmail.com>
2026-01-29 21:46:19 +0530

    trans schm

---
+++ Paytm-Elite/backend/database/transaction.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Account = require("./db");
+
+const transferFund = async(fromAccountId,toAccountId,amount) => {
+    //debut from acc
+    await Account.findByIdAndUpdate(fromAccountId,{$inc : {balance: -amount}});
+
+    //credit to acc
+    await Account.findByIdAndUpdate(toAccountId,{$inc : {balance: amount}});
+}
+
+transferFund('fromAccountId','toAccountId',100);
