0299c24b59ae1eefe7fdab6120536254dde10ce6
Rishab0323 <srishbh574@gmail.com>
2026-02-04 21:47:13 +0530

    signup  route

---
+++ Paytm-Elite/frontend/src/Pages/signup.jsx
@@ -0,0 +1,8 @@
+export const  Signup = () => {
+return <div className="bg-slate-300 h-screen flex justify-center">
+        <div className="rouded-lg bg-white w-80 text-center p-2 h-max px-4y">
+
+        </div>
+    </div>
+    
+}
