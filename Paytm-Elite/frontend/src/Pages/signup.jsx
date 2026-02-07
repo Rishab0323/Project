8fa0715b5423d5cc36496f8e49ceef22b0e42b27
Rishab0323 <srishbh574@gmail.com>
2026-02-07 15:57:49 +0530

    p-2 err

--- Paytm-Elite/frontend/src/Pages/signup.jsx
+++ Paytm-Elite/frontend/src/Pages/signup.jsx
@@ -1,9 +1,24 @@
-export default Signup = () => {
+import { Heading } from "../Component/heading";
+import { Subheading } from "../Component/subheading";
+import { InputBox } from "../Component/InputBox";
+import { Button } from "../Component/button";
+
+const Signup = () => {
     return (
-    <div className="bg-slate-300 h-screen flex justify-center">
-            <div className="rouded-lg bg-white w-80 text-center p-2 h-max px-4y">
-                signup page
+    <div className="bg-red-300 h-screen flex justify-center">
+        <div className="flex flex-col justify-center">
+            <div className="rounded-lg bg-white w-80 text-center p-2 h-max space-y-2">
+                <Heading label={"Signup"}></Heading>
+                <Subheading label={"enter you deatils to create account"} />
+                <InputBox placeholder="John" label={"First Name"}/>
+                <InputBox placeholder="Wick" label={"Last Name"}/>
+                <InputBox placeholder="john@4gmail.com" label={"Email"}/>
+                <InputBox placeholder="12344#*45" label={"Password"}/>
+                <Button label={"Signup"} />
             </div>
+        </div>
     </div>
     
 )}
+
+export default Signup;
