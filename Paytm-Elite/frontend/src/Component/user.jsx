0ea0786dfc293e1aad3ce9bbcd39514daa399d0a
Rishab0323 <srishbh574@gmail.com>
2026-02-17 21:41:15 +0530

    all user

--- Paytm-Elite/frontend/src/Component/user.jsx
+++ Paytm-Elite/frontend/src/Component/user.jsx
@@ -1,12 +1,17 @@
-import {useState}  from "react"
+import {useEffect, useState}  from "react"
 import { Button } from "./button"
+import axios from "axios";
 
 export const User = () => {
-    const [users,setUsers] = useState([{
-        firstname:"",
-        lastname : "",
-        _id: 1
-    }]);
+    const [users,setUsers] = useState([]);
+    const [filter,setFilter] = useState("");
+
+    useEffect(() => {
+        axios.get("http://localhost:2000/api/user/bulk?filter" +filter)
+        .then( response => {
+            setUsers(response.data.user)
+        })
+    },[filter])
 
     return<>
         <div className="font-bold mt-6 text-lg">
@@ -19,4 +24,20 @@ export const User = () => {
             {users.map(user => <User user={user} />)}
         </div>
     </>
+}
+
+function User({user}){
+    return <div className="flex justify-between">
+        <div className="flex">
+            <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
+                <div className="flex flex-col justify-center h-full text-xl">
+                    {user.firstName[0]}
+                </div>
+            </div>
+            <div>
+                {user.firstName} {user.lastName}
+            </div>
+        </div>
+    </div>
+
 }
